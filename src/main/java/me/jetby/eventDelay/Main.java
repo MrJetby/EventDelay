be7b14e556f876d5a90133c1c2f6df8d81edaf72
milim <milimon4ikyt@gmail.com>
2025-04-26 12:32:37 -0400

    2.6.2

--- src/main/java/me/jetby/eventDelay/Main.java
+++ src/main/java/me/jetby/eventDelay/Main.java
@@ -1,52 +1,67 @@
 package me.jetby.eventDelay;
 
+import lombok.Getter;
+import lombok.Setter;
+import me.jetby.eventDelay.commands.EventCMD;
+import me.jetby.eventDelay.commands.TabCompleter;
 import me.jetby.eventDelay.configurations.Config;
 import me.jetby.eventDelay.configurations.Messages;
 import me.jetby.eventDelay.configurations.WebhookConfig;
+import me.jetby.eventDelay.manager.Timer;
+import me.jetby.eventDelay.manager.Triggers;
 import me.jetby.eventDelay.tools.EventDelayAPI;
 import me.jetby.eventDelay.tools.License;
+import me.jetby.eventDelay.tools.PlaceholderAPI;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 import org.bstats.bukkit.Metrics;
 
-public final class Main extends JavaPlugin {
-    public static YamlConfiguration messages;
-    private static Main INSTANCE;
+import java.util.logging.Level;
 
-    public static Main getInstance() {
-        return INSTANCE;
-    }
+import static me.jetby.eventDelay.configurations.Config.CFG;
+
+@Getter
+public final class Main extends JavaPlugin {
+    public static Main INSTANCE;
+    @Setter
+    private EventDelayAPI eventDelayAPI;
+    @Setter
+    private License license;
+    @Setter
+    private EventCMD eventCMD;
+    @Setter
+    private Timer timer;
+    @Setter
+    private Triggers triggers;
 
     @Override
     public void onEnable() {
         INSTANCE = this;
 
-        Config config = new Config();
-        config.loadYamlFile(this);
 
-        Messages messages = new Messages();
-        messages.loadYamlFile(this);
-
-        WebhookConfig webhookConfig = new WebhookConfig();
-        webhookConfig.loadYamlFile(this);
 
         new Metrics(this, 23730);
 
         Bukkit.getServer().getConsoleSender().sendMessage("-------> Login <-------");
-        License license = new License(getConfig().getString("license"), getConfig().getString("licenseLink", "http://treexstudio.site"), this);
+        license = new License(getConfig().getString("license.key"), getConfig().getString("license.url", "http://treexstudio.site"), this);
         license.request();
         Bukkit.getServer().getConsoleSender().sendMessage(" |- License checking: "+license.getLicense());
         if (license.isValid()) {
 
-            Bukkit.getServer().getConsoleSender().sendMessage("------------------------");
-            Bukkit.getServer().getConsoleSender().sendMessage(" |- Login accepted");
-            Bukkit.getServer().getConsoleSender().sendMessage(" |- Welcome: "+license.getLicensedTo());
-            Bukkit.getServer().getConsoleSender().sendMessage(" |- I am enabling all");
-            Bukkit.getServer().getConsoleSender().sendMessage(" |- the features for you");
-            Bukkit.getServer().getConsoleSender().sendMessage("------------------------");
+
+            if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+                new PlaceholderAPI().register();
+            }
+
+
+
+            getLogger().log(Level.INFO, "------------------------");
+            getLogger().log(Level.INFO, " (✔) Лицензия действительна");
+            getLogger().log(Level.INFO, "------------------------");
+            getLogger().log(Level.INFO, " |- Добро пожаловать: "+license.getLicensedTo());
+            getLogger().log(Level.INFO, "------------------------");
 
         } else {
             Bukkit.getServer().getConsoleSender().sendMessage("------------------------");
@@ -60,8 +75,46 @@ public final class Main extends JavaPlugin {
     }
 
 
-    public EventDelayAPI getAPI() {
-        return new EventDelayAPI();
+    @Override
+    public void onLoad() {
+        Config config = new Config();
+        config.loadYamlFile(this);
+
+        Messages messages = new Messages();
+        messages.loadYamlFile(this);
+
+        WebhookConfig webhookConfig = new WebhookConfig();
+        webhookConfig.loadYamlFile(this);
+
+        EventDelayAPI eventDelayAPI = new EventDelayAPI(CFG().getBoolean("Freeze", true),
+                CFG().getInt("Timer", 1800),
+                CFG().getInt("MinPlayers", 3),
+                "none",
+                "none",
+                "none");
+
+        setEventDelayAPI(eventDelayAPI);
+
+        Triggers triggers = new Triggers(eventDelayAPI);
+        setTriggers(triggers);
+
+        Timer timer = new Timer(eventDelayAPI, triggers);
+        setTimer(timer);
+
+
+        getTimer().initialize();
+        getTimer().startTimer();
+
+        EventCMD eventCMD = new EventCMD(eventDelayAPI, timer, triggers);
+        setEventCMD(eventCMD);
+        getCommand("event").setExecutor(getEventCMD());
+        getCommand("event").setTabCompleter(new TabCompleter());
+
+        triggers.nextRandomEvent();
     }
+
+
+
+
 
 }
